af2ba92031f7f6c4e9460e0bdadfbf6d6ad34f38
rerysonn <rerysonfarinha@outlook.com>
2023-10-03 23:36:44 -0300

    User insert - POST

--- src/main/java/com/springboot/workshopmongo/services/UserService.java
+++ src/main/java/com/springboot/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.springboot.workshopmongo.domain.User;
+import com.springboot.workshopmongo.dto.UserDTO;
 import com.springboot.workshopmongo.repository.UserRepository;
 import com.springboot.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -15,14 +16,26 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-
+	
+	//ENCONTRAR TODOS OS USUARIOS
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-
+	
+	// ENCONTRAR O USUARIO PELO ID
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
-
+	}
+	
+	//INSERIR USUARIO
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),
+				objDto.getName(),
+				objDto.getEmail());
 	}
 }

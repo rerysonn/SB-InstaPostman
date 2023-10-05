17e70908b3713b79291ab8bb983c933b53936cf8
rerysonn <rerysonfarinha@outlook.com>
2023-10-04 23:15:07 -0300

    Find post by id

---
+++ src/main/java/com/springboot/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.springboot.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springboot.workshopmongo.domain.Post;
+import com.springboot.workshopmongo.repository.PostRepository;
+import com.springboot.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;
+	
+	
+	
+	// ENCONTRAR O POST PELO ID
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}	
+}

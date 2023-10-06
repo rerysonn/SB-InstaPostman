9dd324eab31866322f1db718733dc4a8c167b0c7
rerysonn <rerysonfarinha@outlook.com>
2023-10-06 10:47:17 -0300

    Added anotation Query for basic query.

--- src/main/java/com/springboot/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/springboot/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.springboot.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.springboot.workshopmongo.domain.Post;
@@ -10,6 +11,11 @@ import com.springboot.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	@Query("{ 'Title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle (String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	
 	
 }
